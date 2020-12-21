91b23727e6d61e54bdc10ac237cc182adf83cecf
Christian Fernandez <cdferdez@gmail.com>
2020-12-20 21:25:05 -0600

    able to build

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
-
-	"github.com/cdferdez/go-torrent/torrentfile"
 )
 
 func main() {
 	inPath := os.Args[1]
-	outPath := os.Args[2]
+	//outPath := os.Args[2]
 
-	tf, err := torrentfile.Open(inPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = tf.DownloadToFile(outPath)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Print(inPath)
 }
